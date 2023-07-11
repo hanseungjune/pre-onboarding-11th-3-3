6df50d10cf6bb25f710c0868a5e3b0358afcd323
team7 <hnhyerim@gmail.com>
2023-07-11 23:11:35 +0900

    Env: color 상수 생성

---
+++ src/constants/color.ts
@@ -0,0 +1,10 @@
+const COLOR = {
+  DarkHeaderBg: '#010409',
+  DarkBodyBg: '#0E1117',
+  DarkBorder: '#21262d',
+  DarkText: '#E6EDF3',
+  DarkGray: '#7E8590',
+  LocalNavFocus: '#F08064',
+};
+
+export default COLOR;
