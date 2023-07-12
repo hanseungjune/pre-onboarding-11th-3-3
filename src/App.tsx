befdc1e9960a27422bfc5f718a3df1c67534841d
team7 <hnhyerim@gmail.com>
2023-07-13 07:20:42 +0900

    Feat: Context Api 생성

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,21 @@ import GlobalStyle from './GlobalStyle';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
+import { IssueApi } from 'apis/issueApi';
+import { HttpClient } from 'httpClient/httpClient';
+import IssueProvider from 'context/IssueContext';
+
+const httpClient = new HttpClient(process.env.REACT_APP_BASE_URL);
+const issueApi = new IssueApi(httpClient);
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <Header />
-      <Outlet />
+      <IssueProvider issueApi={issueApi}>
+        <Header />
+        <Outlet />
+      </IssueProvider>
     </>
   );
 }
