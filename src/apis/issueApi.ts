34e2e8273e21f218336e54ab35841964eff41de7
team7 <hnhyerim@gmail.com>
2023-07-14 21:16:07 +0900

    Refactor: js -> ts로 변경

---
+++ src/apis/issueApi.ts
@@ -0,0 +1,35 @@
+const url = '/repos/facebook/react';
+
+export class IssueApi {
+  private httpClient: any;
+
+  constructor(httpClient: any) {
+    this.httpClient = httpClient;
+  }
+
+  async repository() {
+    const response = await this.httpClient.fetch(url, {
+      method: 'GET',
+    });
+    return response.json();
+  }
+
+  async issueList(page: number) {
+    const response = await this.httpClient.fetch(
+      `${url}/issues?sort=comments&per_page=15&page=${page}`,
+      {
+        method: 'GET',
+      }
+    );
+    return response.json();
+  }
+
+  async issueDetail(issueNum: number) {
+    const response = await this.httpClient.fetch(`${url}/issues${issueNum}`, {
+      method: 'GET',
+    });
+    if (response.status === 200) {
+      return response.json();
+    }
+  }
+}
